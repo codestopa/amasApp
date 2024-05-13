0296f1eab07184aeb0c644c2b17145c71b017cad
Carlos Gallego <carlos.gallego.reina@gmail.com>
2024-05-13 12:35:00 +0200

    Layout Receta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,8 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
